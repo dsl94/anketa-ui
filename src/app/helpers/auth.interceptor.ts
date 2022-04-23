73d4f89811fd23bfaa9f0beb9d3b9c0cb0591865
nemanja <nemanja.petrovic@united.cloud>
2022-04-23 20:35:58 +0200

    User info for admin

--- src/app/helpers/auth.interceptor.ts
+++ src/app/helpers/auth.interceptor.ts
@@ -1,15 +1,30 @@
-import { HTTP_INTERCEPTORS, HttpEvent } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpErrorResponse, HttpEvent} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import {Observable, of, throwError} from 'rxjs';
 import { TokenService } from '../services/token.service';
+import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
+import {catchError} from "rxjs/operators";
 
 const TOKEN_HEADER_KEY = 'Authorization';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private token: TokenService) { }
+  constructor(private token: TokenService, private router: Router, private toastr: ToastrService) { }
+
+  private handleAuthError(err: HttpErrorResponse): Observable<any> {
+    //handle your auth error or rethrow
+    if (err.status === 401 || err.status === 403) {
+      //navigate /delete cookies or whatever
+      this.toastr.error("Your token has expired, please login again");
+      this.router.navigateByUrl(`/login`);
+      // if you've caught / handled the error, you don't want to rethrow it unless you also want downstream consumers to have to handle it as well.
+      return of(err.message); // or EMPTY may be appropriate here
+    }
+    return throwError(err);
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<any> {
     let authReq = req;
@@ -17,7 +32,7 @@ export class AuthInterceptor implements HttpInterceptor {
     if (token != null) {
       authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
     }
-    return next.handle(authReq);
+    return next.handle(authReq).pipe(catchError(x=> this.handleAuthError(x)));
   }
 }
 
