64f5be99e435e82f0b87f19aa309baf0cab8b248
nemanja <nemanja.petrovic@united.cloud>
2022-06-07 17:28:49 +0200

    Kreiranje ankete

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -16,7 +16,7 @@ export class MainComponent implements OnInit {
   form: any = {
     icao: null
   };
-  version: string = '0.1.3'
+  version: string = '0.1.0'
 
   constructor(private tokenService: TokenService, private router: Router) { }
 
